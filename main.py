8561b0a793fd39c787f25b42bd6aaca286c2faed
gedeao <gedeao.sudario@gmail.com>
2022-07-05 22:30:04 -0300

    Exercicio Concluido

---
+++ main.py
@@ -0,0 +1,31 @@
+"Programa em Python que solicite as informações de três pessoas, como nome, idade, peso, altura"
+"Apresenta na tela estas informações de modo que permaneçam alinhados verticalmente com formatação de interpolação"
+
+
+
+
+input ('qual seu nome: ')
+input ('qual a sua idade: ')
+input ('qual o seu peso: ')
+input ('qual a sua altura: ')
+
+input ('usuario 2')
+
+input ('qual seu nome: ')
+input ('qual a sua idade: ')
+input ('qual o seu peso: ')
+input ('qual a sua altura: ')
+
+input ('usuario 3')
+
+input ('qual seu nome: ')
+input ('qual a sua idade: ')
+input ('qual o seu peso: ')
+input ('qual a sua altura: ')
+
+
+
+
+        
+
+
